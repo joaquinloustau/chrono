98ee1782843af9291022d2e8fafbc72815a5237a
Wanasit Tanakitrungruang <berrysboy@gmail.com>
2015-11-03 19:53:09 +0900

    Add isStrictMode() to parsers, add isPossibleCheck() to components

--- src/options.js
+++ src/options.js
@@ -27,24 +27,23 @@ var ExtractTimezoneAbbrRefiner = require('./refiners/ExtractTimezoneAbbrRefiner'
 var UnlikelyFormatFilter = require('./refiners/UnlikelyFormatFilter').Refiner;
 
 
-exports.strictOption = function () {
-    return {
+function baseOption(strictMode) {
 
+    return {
         parsers: [
         
             // EN
-            new ENISOFormatParser(),
-            new ENDeadlineFormatParser(),
-            new ENMonthNameLittleEndianParser(),
-            new ENMonthNameMiddleEndianParser(),
-            new ENSlashDateFormatParser(),
-            new ENSlashDateFormatStartWithYearParser(),
-            new ENTimeAgoFormatParser(),           
-            new ENTimeExpessionParser(),
-            new ENHolidayParser(),
+            new ENISOFormatParser(strictMode),
+            new ENDeadlineFormatParser(strictMode),
+            new ENMonthNameLittleEndianParser(strictMode),
+            new ENMonthNameMiddleEndianParser(strictMode),
+            new ENSlashDateFormatParser(strictMode),
+            new ENSlashDateFormatStartWithYearParser(strictMode),
+            new ENTimeAgoFormatParser(strictMode),           
+            new ENTimeExpessionParser(strictMode),
 
             // JP
-            new JPStandardParser(),
+            new JPStandardParser(strictMode),
         ],
 
         refiners: [
@@ -62,12 +61,19 @@ exports.strictOption = function () {
             new UnlikelyFormatFilter()
         ]
     }
+}
+
+
+
+exports.strictOption = function () {
+    return baseOption(true);
 };
 
 
 exports.casualOption = function () {
 
-    var options = exports.strictOption();
+    var options = baseOption(false);
+    
     // EN
     options.parsers.unshift(new ENCasualDateParser());
     options.parsers.unshift(new ENWeekdayParser());
